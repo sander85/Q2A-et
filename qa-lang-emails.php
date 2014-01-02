c4340fe0b937183941c81a08a022ab1591499d02
Sander Lepik <sander.lepik@eesti.ee>
2014-01-02 15:35:52 +0200

    Initial translation

--- qa-lang-emails.php
+++ qa-lang-emails.php
@@ -2,12 +2,11 @@
 	
 /*
 	Question2Answer (c) Gideon Greenspan
-
 	http://www.question2answer.org/
 
+	Copyright (C) 2014 Sander Lepik
 	
-	File: qa-include/qa-lang-emails.php
-	Version: See define()s at top of qa-include/qa-base.php
+	File: qa-lang/et/qa-lang-emails.php
 	Description: Language phrases for email notifications
 
 
@@ -25,68 +24,68 @@
 */
 
 	return array(
-		'a_commented_body' => "Your answer on ^site_title has a new comment by ^c_handle:\n\n^open^c_content^close\n\nYour answer was:\n\n^open^c_context^close\n\nYou may respond by adding your own comment:\n\n^url\n\nThank you,\n\n^site_title",
-		'a_commented_subject' => 'Your ^site_title answer has a new comment',
+		'a_commented_body' => "Sinu vastus keskkonnas "^site_title" sai kasutajalt ^c_handle uue kommentaari:\n\n^open^c_content^close\n\nSinu vastuseks oli:\n\n^open^c_context^close\n\nVastamiseks võid lisada kommentaari järgmisel lingil:\n\n^url\n\nTäname,\n\n^site_title",
+		'a_commented_subject' => '^site_title: sinu vastusele lisati uus kommentaar',
 
-		'a_followed_body' => "Your answer on ^site_title has a new related question by ^q_handle:\n\n^open^q_title^close\n\nYour answer was:\n\n^open^a_content^close\n\nClick below to answer the new question:\n\n^url\n\nThank you,\n\n^site_title",
-		'a_followed_subject' => 'Your ^site_title answer has a related question',
+		'a_followed_body' => "Kasutaja ^q_handle esitas keskkonnas "^site_title" sinu vastusega seoses uue küsimuse:\n\n^open^q_title^close\n\nSinu vastuseks oli:\n\n^open^a_content^close\n\nUuele küsimusele vastamiseks klõpsa järgneval lingil:\n\n^url\n\nTäname,\n\n^site_title",
+		'a_followed_subject' => '^site_title: sinu vastusega seoses esitati uus küsimus',
 
-		'a_selected_body' => "Congratulations! Your answer on ^site_title has been selected as the best by ^s_handle:\n\n^open^a_content^close\n\nThe question was:\n\n^open^q_title^close\n\nClick below to see your answer:\n\n^url\n\nThank you,\n\n^site_title",
-		'a_selected_subject' => 'Your ^site_title answer has been selected!',
+		'a_selected_body' => "Õnnitlused! Kasutaja ^s_handle valis keskonnas "^site_title" sinu vastuse parimaks:\n\n^open^a_content^close\n\nKüsimuseks oli:\n\n^open^q_title^close\n\nOma vastuse nägemiseks klõpsa järgneval lingil:\n\n^url\n\nTäname,\n\n^site_title",
+		'a_selected_subject' => '^site_title: sinu vastus valiti parimaks!',
 
-		'c_commented_body' => "A new comment by ^c_handle has been added after your comment on ^site_title:\n\n^open^c_content^close\n\nThe discussion is following:\n\n^open^c_context^close\n\nYou may respond by adding another comment:\n\n^url\n\nThank you,\n\n^site_title",
-		'c_commented_subject' => 'Your ^site_title comment has been added to',
+		'c_commented_body' => "Kasutaja ^c_handle lisas keskkonnas "^site_title" sinu kommentaari järele uue kommentaari:\n\n^open^c_content^close\n\nArutelu teema:\n\n^open^c_context^close\n\nKommentaariga vastamiseks klõpsa järgneval lingil:\n\n^url\n\nTäname,\n\n^site_title",
+		'c_commented_subject' => '^site_title: sinu kommentaari järele lisati uus kommentaar',
 
-		'confirm_body' => "Please click below to confirm your email address for ^site_title.\n\n^url\n\nThank you,\n^site_title",
-		'confirm_subject' => '^site_title - Email Address Confirmation',
+		'confirm_body' => "Et kinnitada oma e-posti aadress keskkonnas "^site_title", klõpsa järgneval lingil:\n\n^url\n\nTäname,\n^site_title",
+		'confirm_subject' => '^site_title: e-posti aadressi kinnitamine',
 
-		'feedback_body' => "Comments:\n^message\n\nName:\n^name\n\nEmail:\n^email\n\nPrevious page:\n^previous\n\nUser:\n^url\n\nIP address:\n^ip\n\nBrowser:\n^browser",
-		'feedback_subject' => '^ feedback',
+		'feedback_body' => "Kommentaarid:\n^message\n\nNimi:\n^name\n\nE-posti aadress:\n^email\n\nEelmine külastatud leht:\n^previous\n\nKasutaja:\n^url\n\nIP-aadress:\n^ip\n\nBrauser:\n^browser",
+		'feedback_subject' => '^: tagasiside',
 
-		'flagged_body' => "A post by ^p_handle has received ^flags:\n\n^open^p_context^close\n\nClick below to see the post:\n\n^url\n\n\nClick below to review all flagged posts:\n\n^a_url\n\n\nThank you,\n\n^site_title",
-		'flagged_subject' => '^site_title has a flagged post',
+		'flagged_body' => "Kasutaja ^p_handle postitusele on lisatud ^flags:\n\n^open^p_context^close\n\nPostituse vaatamiseks klõpsa järgneval lingil:\n\n^url\n\n\nKõigi märgistatud postituste nägemiseks klõpsa alloleval lingil:\n\n^a_url\n\n\nTäname,\n\n^site_title",
+		'flagged_subject' => '^site_title: teavitati postitusest',
 
-		'moderate_body' => "A post by ^p_handle requires your approval:\n\n^open^p_context^close\n\nClick below to approve or reject the post:\n\n^url\n\n\nClick below to review all queued posts:\n\n^a_url\n\n\nThank you,\n\n^site_title",
-		'moderate_subject' => '^site_title moderation',
+		'moderate_body' => "Kasutaja ^p_handle postitus vajab heakskiitu:\n\n^open^p_context^close\n\nPostituse heakskiitmiseks või tagasilükkamiseks klõpsa järgneval lingil:\n\n^url\n\n\nKõigi heakskiitu ootavate postituste nägemiseks klõpsa alloleval lingil:\n\n^a_url\n\n\nTäname,\n\n^site_title",
+		'moderate_subject' => '^site_title: modereerimine',
 
-		'new_password_body' => "Your new password for ^site_title is below.\n\nPassword: ^password\n\nIt is recommended to change this password immediately after logging in.\n\nThank you,\n^site_title\n^url",
-		'new_password_subject' => '^site_title - Your New Password',
+		'new_password_body' => "Uue parooli keskkonna "^site_title" jaoks leiad altpoolt.\n\nParool: ^password\n\nOn tungivalt soovitatav, et muudaksid selle parooli kohe pärast sisselogimist.\n\nTäname,\n^site_title\n^url",
+		'new_password_subject' => '^site_title: uus parool',
 
-		'private_message_body' => "You have been sent a private message by ^f_handle on ^site_title:\n\n^open^message^close\n\n^moreThank you,\n\n^site_title\n\n\nTo block private messages, visit your account page:\n^a_url",
-		'private_message_info' => "More information about ^f_handle:\n\n^url\n\n",
-		'private_message_reply' => "Click below to reply to ^f_handle by private message:\n\n^url\n\n",
-		'private_message_subject' => 'Message from ^f_handle on ^site_title',
+		'private_message_body' => "Kasutaja ^f_handle saatis sulle keskkonnas "^site_title" uue privaatsõnumi:\n\n^open^message^close\n\n^moreTäname,\n\n^site_title\n\n\nPrivaatsõnumite blokeerimiseks külasta oma konto lehte järgneval lingil:\n^a_url",
+		'private_message_info' => "Rohkem infot kasutaja ^f_handle kohta:\n\n^url\n\n",
+		'private_message_reply' => "Kasutajale ^f_handle privaatsõnumiga vastamiseks klõpsa järgneval lingil:\n\n^url\n\n",
+		'private_message_subject' => '^site_title: privaatsõnum kasutajalt ^f_handle',
 
-		'q_answered_body' => "Your question on ^site_title has been answered by ^a_handle:\n\n^open^a_content^close\n\nYour question was:\n\n^open^q_title^close\n\nIf you like this answer, you may select it as the best:\n\n^url\n\nThank you,\n\n^site_title",
-		'q_answered_subject' => 'Your ^site_title question was answered',
+		'q_answered_body' => "Kasutaja ^a_handle vastas keskkonnas "^site_title" sinu küsimusele:\n\n^open^a_content^close\n\nSinu küsimuseks oli:\n\n^open^q_title^close\n\nKui see küsimus meeldib sulle, siis võid selle valida parimaks:\n\n^url\n\nTäname,\n\n^site_title",
+		'q_answered_subject' => '^site_title: sinu küsimusele vastati',
 
-		'q_commented_body' => "Your question on ^site_title has a new comment by ^c_handle:\n\n^open^c_content^close\n\nYour question was:\n\n^open^c_context^close\n\nYou may respond by adding your own comment:\n\n^url\n\nThank you,\n\n^site_title",
-		'q_commented_subject' => 'Your ^site_title question has a new comment',
+		'q_commented_body' => "Kasutaja ^c_handle lisas keskkonnas "^site_title" sinu küsimusele uue kommentaari:\n\n^open^c_content^close\n\nSinu küsimuseks oli:\n\n^open^c_context^close\n\nSa võid kommentaarile oma kommentaariga vastata:\n\n^url\n\nTäname,\n\n^site_title",
+		'q_commented_subject' => '^site_title: sinu küsimusele lisati kommentaar',
 
-		'q_posted_body' => "A new question has been asked by ^q_handle:\n\n^open^q_title\n\n^q_content^close\n\nClick below to see the question:\n\n^url\n\nThank you,\n\n^site_title",
-		'q_posted_subject' => '^site_title has a new question',
+		'q_posted_body' => "Kasutaja ^q_handle esitas uue küsimuse:\n\n^open^q_title\n\n^q_content^close\n\nKüsimuse vaatamiseks klõpsa järgneval lingil:\n\n^url\n\nTäname,\n\n^site_title",
+		'q_posted_subject' => '^site_title: esitati uus küsimus',
 		
-		'remoderate_body' => "An edited post by ^p_handle requires your reapproval:\n\n^open^p_context^close\n\nClick below to approve or hide the edited post:\n\n^url\n\n\nClick below to review all queued posts:\n\n^a_url\n\n\nThank you,\n\n^site_title",
-		'remoderate_subject' => '^site_title moderation',
+		'remoderate_body' => "Kasutaja ^p_handle muudetud postitus vajab heakskiitu:\n\n^open^p_context^close\n\nPostituse heakskiitmiseks või peitmiseks klõpsa järgneval lingil:\n\n^url\n\n\nKõigi heakskiitu vajavate postituste vaatamiseks klõpsa alloleval lingil:\n\n^a_url\n\n\nTäname,\n\n^site_title",
+		'remoderate_subject' => '^site_title: modereerimine',
 
-		'reset_body' => "Please click below to reset your password for ^site_title.\n\n^url\n\nAlternatively, enter the code below into the field provided.\n\nCode: ^code\n\nIf you did not ask to reset your password, please ignore this message.\n\nThank you,\n^site_title",
-		'reset_subject' => '^site_title - Reset Forgotten Password',
+		'reset_body' => "Keskkonna "^site_title" parooli lähtestamiseks klõpsa järgneval lingil:\n\n^url\n\nAlternatiivina võid sisestada alloleva koodi vastavasse vormi välja.\n\nKood: ^code\n\nKui sa ei soovinud parooli lähtestamist, siis ignoreeri seda kirja.\n\nTäname,\n^site_title",
+		'reset_subject' => '^site_title: unustatud parooli lähtestamine',
 
-		'to_handle_prefix' => "^,\n\n",
+		'to_handle_prefix' => "Tere, ^!\n\n",
 		
-		'u_registered_body' => "A new user has registered as ^u_handle.\n\nClick below to view the user profile:\n\n^url\n\nThank you,\n\n^site_title",
-		'u_to_approve_body' => "A new user has registered as ^u_handle.\n\nClick below to approve the user:\n\n^url\n\nClick below to review all users waiting for approval:\n\n^a_url\n\nThank you,\n\n^site_title",
-		'u_registered_subject' => '^site_title has a new registered user',
+		'u_registered_body' => "Registreerus uus kasutaja: ^u_handle.\n\nKasutaja profiili vaatamiseks klõpsa järgneval lingil:\n\n^url\n\nTäname,\n\n^site_title",
+		'u_to_approve_body' => "Registreerus uus kasutaja: ^u_handle.\n\nKasutaja heakskiitmiseks klõpsa järgneval lingil:\n\n^url\n\nKõigi ootel olevate kasutajate vaatamiseks klõpsa alloleval lingil:\n\n^a_url\n\nTäname,\n\n^site_title",
+		'u_registered_subject' => '^site_title: registreerus uus kasutaja',
 		
-		'u_approved_body' => "You can see your new user profile here:\n\n^url\n\nThank you,\n\n^site_title",
-		'u_approved_subject' => 'Your ^site_title user has been approved',
+		'u_approved_body' => "Võid oma uut profiili vaadata siit:\n\n^url\n\nTäname,\n\n^site_title",
+		'u_approved_subject' => '^site_title: sinu kasutaja sai heakskiidu',
 		
-		'wall_post_subject' => 'Post on your ^site_title wall',
-		'wall_post_body' => "^f_handle has posted on your user wall at ^site_title:\n\n^open^post^close\n\nYou may respond to the post here:\n\n^url\n\nThank you,\n\n^site_title",
+		'wall_post_subject' => '^site_title: postitati seinale',
+		'wall_post_body' => "Kasutaja ^f_handle postitas keskkonnas "^site_title" sinu kasutaja seinale:\n\n^open^post^close\n\nVõid postitusele vastata järgneva lingi kaudu:\n\n^url\n\nTäname,\n\n^site_title",
 
-		'welcome_body' => "Thank you for registering for ^site_title.\n\n^custom^confirmYour login details are as follows:\n\nEmail: ^email\nPassword: ^password\n\nPlease keep this information safe for future reference.\n\nThank you,\n\n^site_title\n^url",
-		'welcome_confirm' => "Please click below to confirm your email address.\n\n^url\n\n",
-		'welcome_subject' => 'Welcome to ^site_title!',
+		'welcome_body' => "Täname, et registreerusid keskkonnas "^site_title".\n\n^custom^confirmSinu andmed on järgmised:\n\nE-posti aadress: ^email\n\nTäname,\n\n^site_title\n^url",
+		'welcome_confirm' => "E-posti aadressi kinnitamiseks klõpsa järgneval lingil:\n\n^url\n\n",
+		'welcome_subject' => 'Tere tulemast keskkonda "^site_title"!',
 	);
 	
 
